6eec35fe465d3f6b31936fab7cd6c10b917d95af
Vasiliy Golban <vgolban@accesssoftek.com>
2021-03-10 11:38:51 +0200

    Add login ui test

--- src/test/java/tests/AppTests.java
+++ src/test/java/tests/AppTests.java
@@ -2,10 +2,23 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class AppTests {
 
     @Test
-    void LoginTest(){
+    void UserLogin() {
+        open("https://godzila.bg/en/");
+        $(".popup-btn-city").click();
+        $(byText("login")).click();
+        $("#login-email").setValue("testaqa@protonmail.com");
+        $("input[name=password]").setValue("testaqa1");
+        $(".popup.show button.popup-btn-login").click();
 
+        $(byText("logout")).shouldBe(visible, Duration.ofSeconds(30));
     }
 }
