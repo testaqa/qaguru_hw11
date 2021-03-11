99cd6359527fd938de24805a69feb689eee8ed6d
Vasiliy Golban <vgolban@accesssoftek.com>
2021-03-11 08:01:51 +0200

    Add DriverHelper

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import config.ConfigHelper;
+import helpers.DriverHelper;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.HomePage;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -14,18 +12,8 @@ public class BaseTest {
 
     @BeforeAll
     public static void BeforeAll(){
-        Configuration.baseUrl = ConfigHelper.getWebUrl();
-        Configuration.browser = System.getProperty("browser", "chrome");
-var test = ConfigHelper.isRemoteWebDriver();
 
-        if (ConfigHelper.isRemoteWebDriver()) {
-            Configuration.remote = ConfigHelper.getWebRemoteDriver();
-
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-        }
+        DriverHelper.configureDriver();
 
         open("");
         HomePage homePage = new HomePage();
